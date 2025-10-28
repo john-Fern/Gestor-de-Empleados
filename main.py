0d850fd30f0f5c92ab73caa38e8dc8a287af476d
john <jonathan.fernandez.f@usach.cl>
2025-10-28 16:33:36 -0300

    dejar vacio main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-def ingresar_datos():
-    nombre = input("Ingrese su nombre: ")
-    edad = int(input("Ingrese su edad: "))
-    cargo = input("Ingrese su cargo: ")
-    sueldo_base = int(input("Ingrese sueldo base: "))
-    porcentaje_bono = float(int("Ingrese porcentaje bono: "))
