e29748e9de559ed93079db3d61a4a1bec5c0289b
Martin Caviedes <martincn2323@gmail.com>
2025-10-28 17:01:37 -0300

    docs(resumen.py): FuncionMostrarResumen

---
+++ resumen.py
@@ -0,0 +1,10 @@
+def mostrarResumen(nombre, edad, cargo, total):
+    print("================================")
+    print("\tRESUMEN EMPLEADO")
+    print("================================")
+    print(f"Nombre Empleado: {nombre}")
+    print(f"Edad Empleado: {edad}")
+    print(f"Cargo Empleado: {cargo}") 
+    print(f"Sueldo final Empleado: {total}")
+
+ 
