0d2b2dd261bdb940f2b3e9e29540a9ae7a640ed8
Nicolás Peña <nicolas.2244@hotmail.com>
2025-10-28 23:32:38 -0300

    docs(datos.py): updatededatos.py
    Se modifico el texto de datos.py

--- datos.py
+++ datos.py
@@ -1,10 +1,27 @@
 def ingresar_datos():
     print("\n=== Registro de Empleado ===")
 
-    nombre = str(input("Ingrese el nombre del empleado: "))
-    edad = int(input("Ingrese la edad del empleado: "))
-    cargo = str(input("Ingrese el cargo del empleado: "))
-    sueldo_base = int(input("Ingrese el sueldo base del empleado: "))
-    porcentaje_bono = float(input("Ingrese el porcentaje de bono (%): "))
+    nombre = input("Nombre: ").strip()
+    while nombre == "" or nombre[0] >= "0" and nombre[0] <= "9":
+        nombre = input("❎ Ingrese un nombre válido: ").strip()
 
-    return nombre, edad, cargo, sueldo_base, porcentaje_bono
+    edad = input("Edad: ").strip()
+    while edad == "" or edad[0] < "0" or edad[0] > "9" or int(edad) < 18:
+        edad = input("❎ Ingrese una edad válida (18 o más): ").strip()
+    edad = int(edad)
+
+    cargo = input("Cargo: ").strip()
+    while cargo == "" or cargo[0] >= "0" and cargo[0] <= "9":
+        cargo = input("❎ Ingrese un cargo válido: ").strip()
+
+    sueldo = input("Sueldo base: ").strip()
+    while sueldo == "" or float(sueldo) <= 0:
+        sueldo = input("❎ Ingrese un sueldo mayor que 0: ").strip()
+    sueldo = float(sueldo)
+
+    bono = input("Porcentaje de bono: ").strip()
+    while bono == "" or float(bono) <= 0:
+        bono = input("❎ Ingrese un porcentaje mayor que 0: ").strip()
+    bono = float(bono)
+
+    return nombre, edad, cargo, sueldo, bono
